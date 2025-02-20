8c785b3bb828cafced4371f833965e6a09c7269e
felipe.r.moran@gmail.com <felipe.r.moran@gmail.com>
2025-02-20 14:19:02 -0300

    Specialty_management_screen

--- fila_postinho_front/lib/screens/specialty/specialty_management_screen.dart
+++ fila_postinho_front/lib/screens/specialty/specialty_management_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:fila_postinho_front/widgets/theme_toggle_button.dart';
 import 'package:fila_postinho_front/widgets/background_gradient.dart';
+import 'package:fila_postinho_front/services/specialty_service.dart';
+import 'package:fila_postinho_front/models/specialty_model.dart';
+import 'package:fila_postinho_front/services/auth_storage_service.dart';
 
 class SpecialtyManagementScreen extends StatefulWidget {
   final VoidCallback toggleTheme;
@@ -13,45 +17,88 @@ class SpecialtyManagementScreen extends StatefulWidget {
 }
 
 class SpecialtyManagementScreenState extends State<SpecialtyManagementScreen> {
-  final List<String> specialties = [];
+  late Future<List<Specialty>> _specialtiesFuture;
   final TextEditingController _specialtyController = TextEditingController();
+  String token = "";
 
-  void _addSpecialty() {
-    if (_specialtyController.text.isNotEmpty) {
-      setState(() {
-        specialties.add(_specialtyController.text);
-        _specialtyController.clear();
-      });
-    }
+  @override
+  void initState() {
+    super.initState();
+    _loadToken();
   }
 
-  void _removeSpecialty(int index) {
+  Future<void> _loadToken() async {
+    String? savedToken = await AuthStorageService.getToken();
     setState(() {
-      specialties.removeAt(index);
+      token = savedToken ?? "";
+      _specialtiesFuture = _fetchSpecialties();
     });
   }
 
-  void _editSpecialty(int index) {
-    _specialtyController.text = specialties[index];
+  Future<List<Specialty>> _fetchSpecialties() async {
+    if (token.isEmpty) return [];
+    final specialtyService =
+        Provider.of<SpecialtyService>(context, listen: false);
+    try {
+      return await specialtyService.findAll(token);
+    } catch (e) {
+      throw Exception('Não foi possível carregar as especialidades: $e');
+    }
+  }
+
+  void _addSpecialty() async {
+    if (_specialtyController.text.isEmpty) return;
+    final newSpecialty = Specialty(specialtyName: _specialtyController.text);
+    try {
+      final specialtyService =
+          Provider.of<SpecialtyService>(context, listen: false);
+      await specialtyService.create(newSpecialty);
+      _specialtyController.clear();
+      setState(() {
+        _specialtiesFuture = _fetchSpecialties();
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro ao adicionar: $e')),
+      );
+    }
+  }
+
+  void _editSpecialty(Specialty specialty) {
+    _specialtyController.text = specialty.specialtyName;
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: Text('Editar Especialidade'),
+          title: const Text('Editar Especialidade'),
           content: TextField(
             controller: _specialtyController,
-            decoration: InputDecoration(labelText: 'Especialidade'),
+            decoration: const InputDecoration(labelText: 'Especialidade'),
           ),
           actions: [
             TextButton(
-              onPressed: () {
-                setState(() {
-                  specialties[index] = _specialtyController.text;
+              onPressed: () async {
+                try {
+                  final updatedSpecialty = Specialty(
+                    specialtyId: specialty.specialtyId,
+                    specialtyName: _specialtyController.text,
+                  );
+                  final specialtyService =
+                      Provider.of<SpecialtyService>(context, listen: false);
+                  await specialtyService.update(
+                      specialty.specialtyId.toString(), updatedSpecialty);
                   _specialtyController.clear();
-                });
-                Navigator.of(context).pop();
+                  setState(() {
+                    _specialtiesFuture = _fetchSpecialties();
+                  });
+                  Navigator.of(context).pop();
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Erro ao editar: $e')),
+                  );
+                }
               },
-              child: Text('Salvar'),
+              child: const Text('Salvar'),
             ),
           ],
         );
@@ -59,12 +106,27 @@ class SpecialtyManagementScreenState extends State<SpecialtyManagementScreen> {
     );
   }
 
+  void _deleteSpecialty(String id) async {
+    try {
+      final specialtyService =
+          Provider.of<SpecialtyService>(context, listen: false);
+      await specialtyService.delete(id);
+      setState(() {
+        _specialtiesFuture = _fetchSpecialties();
+      });
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro ao excluir: $e')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return BackgroundGradient(
       child: Scaffold(
         appBar: AppBar(
-          title: Text('Gerenciamento de Especialidades'),
+          title: const Text('Gerenciamento de Especialidades'),
           actions: [
             ThemeToggleButton(
               onPressed: widget.toggleTheme,
@@ -78,34 +140,54 @@ class SpecialtyManagementScreenState extends State<SpecialtyManagementScreen> {
             children: [
               TextField(
                 controller: _specialtyController,
-                decoration: InputDecoration(labelText: 'Nova Especialidade'),
+                decoration:
+                    const InputDecoration(labelText: 'Nova Especialidade'),
               ),
               const SizedBox(height: 10),
               ElevatedButton(
                 onPressed: _addSpecialty,
-                child: Text('Adicionar Especialidade'),
+                child: const Text('Adicionar Especialidade'),
               ),
               const SizedBox(height: 20),
               Expanded(
-                child: ListView.builder(
-                  itemCount: specialties.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(specialties[index]),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            icon: Icon(Icons.edit),
-                            onPressed: () => _editSpecialty(index),
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.delete),
-                            onPressed: () => _removeSpecialty(index),
-                          ),
-                        ],
-                      ),
-                    );
+                child: FutureBuilder<List<Specialty>>(
+                  future: _specialtiesFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      return Center(
+                        child: Text('Erro: ${snapshot.error}'),
+                      );
+                    } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return const Center(
+                          child: Text('Nenhuma especialidade cadastrada'));
+                    } else {
+                      final specialties = snapshot.data!;
+                      return ListView.builder(
+                        itemCount: specialties.length,
+                        itemBuilder: (context, index) {
+                          final specialty = specialties[index];
+                          return ListTile(
+                            title: Text(specialty.specialtyName),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  icon: const Icon(Icons.edit),
+                                  onPressed: () => _editSpecialty(specialty),
+                                ),
+                                IconButton(
+                                  icon: const Icon(Icons.delete),
+                                  onPressed: () => _deleteSpecialty(
+                                      specialty.specialtyId.toString()),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    }
                   },
                 ),
               ),
